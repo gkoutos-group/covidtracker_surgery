e6bd804626c0548704b003deec48960255f004aa
Victor R. Cardoso <labrax@users.noreply.github.com>
2022-01-07 09:08:43 +0000

    file not needed anymore

--- update.sh
+++
@@ -1,6 +0,0 @@
-cd /rds/homes/r/rothcarv/covidtracker_surgery/
-
-curl "https://api.coronavirus.data.gov.uk/v2/data?areaType=nation&areaCode=E92000001&metric=hospitalCases&format=csv" --output coronavirus.csv
-# based on https://coronavirus.data.gov.uk/details/download
-
-python3 20220106_process_number_cases.py
